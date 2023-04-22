cf96df3036770584dcb93ccbefa5bbb5ad8cb93b
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-04-22 17:10:33 -0400

    Update part1.py

--- part1.py
+++ part1.py
@@ -89,6 +89,9 @@ for player, data in p.items():
                 cntds[player][i] = future_centroid[i]
 
 output = sc.parallelize([shooter + '\t' + str(centroid_pos) for shooter, centroid_pos in cntds.items()])
+for i in range(10):
+  print("THE OUTPUT")
+  
 print(output)
 output.saveAsTextFile("hdfs://10.128.0.2:9000/part1/output")
 
