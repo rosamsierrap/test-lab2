9da82896db10490f69f452704aa9b92830b9421f
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-04-22 18:45:14 -0400

    Update part1.py

--- part1.py
+++ part1.py
@@ -112,11 +112,13 @@ for player, data in p.items():
 output = sc.parallelize([shooter + '\t' + str(centroid_pos) for shooter, centroid_pos in cntds.items()])
 for i in range(10):
   print("THE OUTPUT")
-#for player,centroid_pos in cntds.items():
- #   print(player+'\t'+str(centroid_pos))
-    
-print(output.collect())
-#print(output)
-#output.saveAsTextFile("hdfs://10.128.0.2:9000/part1/output")
+
+output.saveAsTextFile("hdfs://$SPARK_MASTER:9000/part1/output")
+# collect the output to driver and write to a file
+output_list = output.collect()
+with open("output.txt", "w") as f:
+    for line in output_list:
+        f.write(line + "\n")
+
 
 sc.stop()
