298741cceee16c63af65cad0ccbb148340ee9c73
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-04-22 16:01:37 -0400

    Create part1.py

---
+++ part1.py
@@ -0,0 +1,95 @@
+# ---- coding:utf-8 ----
+from __future__ import print_function
+import sys
+from pyspark import SparkContext, SparkConf
+
+from operator import add
+from pyspark.sql import SparkSession
+import random
+
+conf = SparkConf().setAppName("PART1")
+sc = SparkContext(conf=conf)
+
+textFile = sc.textFile(sys.argv[1]) # data
+
+info = textFile.flatMap(lambda line: line.split("\n")) #lines
+
+# Map to (shooter, (dist, def_dist, time))
+ShotDistDefdistTime= info.map(lambda line: (line.split(",")[15]+line.split(",")[16].strip('"'),  #shooter
+                                            ((line.split(",")[12]),  #dist
+                                             (line.split(",")[18]), #def_dist
+                                             (line.split(",")[9])))) #time//float
+
+made = ShotDistDefdistTime.filter(lambda pair: pair[1][0] == 'made') #hit==made
+
+made_byshooter = made.groupByKey()
+
+
+p = made_byshooter.map(lambda pair: (pair[0], list(pair[1]))).collectAsMap()
+
+cntd = 4
+cntds = {}
+
+def l2(set1, set2):
+    d = 0
+    for i in range(len(set1)):
+        d += (set1[i] - set2[i]) ** 2
+    return (d ** 0.5)
+
+# For each shooter, perform k-means clustering
+for player, data in p.items():
+    if len(data) < 4:
+        continue
+    if player not in cntds:
+        cntds[player] = []
+    if not cntds[player]:
+        nums = []
+        for _ in range(cntd):
+            nums.append(random.randint(0, len(data) - 1))
+            if len(nums) == 1:
+                continue
+            while nums[-1] == nums[-2]:
+                nums[-1] = random.randint(0, len(data) - 1)
+        for i in nums:
+            cntds[player].append(data[i])
+    # Perform 10 iterations of clustering
+    for _ in range(10):
+        future_centroid = [[] for _ in range(cntd)]
+        for dataset in data:
+            distances = []
+            for centroid_positions in cntds[player]:
+                dst = l2(centroid_positions, dataset)
+                distances.append(dst)
+            min_indx = -1
+            min_val = 100000
+            j = 0
+            for k in distances:
+                if k < min_val:
+                    min_val = k
+                    min_indx = j
+                j += 1
+            future_centroid[min_indx].append(dataset)
+        for ix, f_cntd in enumerate(future_centroid):
+            d = []
+            t = 0
+            try:
+                for t in range(len(f_cntd[0])):
+                    d.append(0)
+                for row in f_cntd:
+                    for k, point in enumerate(row):
+                        d[k] += point
+                    t += 1
+                for indx, point in enumerate(d):
+                    d[indx] = round(d[indx]/t, 4)
+                future_centroid[ix] = d
+            except:
+                pass
+        for i in range(len(cntds[player])):
+            if future_centroid[i]:
+                cntds[player][i] = future_centroid[i]
+
+output = sc.parallelize([shooter + '\t' + str(centroid_pos) for shooter, centroid_pos in cntds.items()])
+print(output)
+output.saveAsTextFile("hdfs://128.10.0.2:9000/part1/output")
+
+sc.stop()
